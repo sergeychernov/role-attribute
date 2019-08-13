9374dd61e5ba5d0c8bf43433072718332558b7d2
Сергей Чернов <chernov.sergey@gmail.com>
2019-08-13 14:57:31 +0300

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ARIAPreDefinedRoles_1 = require("./ARIAPreDefinedRoles");
+exports.ARIAPreDefinedRoles = ARIAPreDefinedRoles_1.ARIAPreDefinedRoles;
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export {ARIAPreDefinedRoles} from'./ARIAPreDefinedRoles';
